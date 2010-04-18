c92286cb7c0d3b0cd9357e089dbc988397e5326e
Ludovic Delaveau <ldelaveau@gmail.com>
2010-04-19 01:01:02 +0200

    New filling method, using the filling percentage to determine wether it's appropriate to fill the cell. Method also pretty simplier ...
    Change all the signatures of the creation method : they no longer
    require the initial variable, and all send back the structure once
    created.
    
    Beginning of the implementation of the conversion method. It's late, so
    I will continue tomorrow.
    
    Have a good night !

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <limits.h>
 #include <time.h>
 
-#define REMPLISSAGE 0.3
+#define FILLING 0.3
 #define INFI_LOCAL 20
 
 int nb_summit = 10;
@@ -12,53 +12,77 @@ int nb_summit = 10;
 /* simple data structure : chained-list containing the number of the summit */
 struct chained_list {
 	unsigned long int numero;
+	int value;
 	struct chained_list * next;
 };
 
 /* function to save the datas in a file */
 
 /* function to convert a matrix into a chained list */
+struct chained_list * convert_matrix(int ** tab) {
+	unsigned long int i, j;
+	struct chained_list * pointer;
+	
+	struct chained_list **list = (struct chained_list **) calloc(nb_summit, sizeof(struct chained_list *));
+	
+	for (i=0; i<nb_summit; i++) {
+		for (j=0; j<nb_summit; j++) {
+			if (j != i && tab[i][j] != 0) {
+				pointer->numero = j;
+				/* have a pointer to the current editing struct, as done in a stack ? */
+				
+			}
+		}
+	}
+	
+	return NULL;
+}
 
-/* function to print the matrix */
-void print_matrix(unsigned long int ** tableau) {
+/* function to convert from chained_list to matrix ? is there a need for it ? */
+
+/* function to print the matrix in the terminal ... simple displaying method */
+void print_matrix(int ** tableau) {
 	
 	int i, j;
 	
 	for (i=0; i<nb_summit; i++) {
 		for (j=0; j<nb_summit; j++) {
-			printf("%ld ", tableau[i][j]);
+			printf("%d ", tableau[i][j]);
 		}
 		printf("\n");
 	}
 }
 
+/* function to print a chained_list, maybe as a tab ? */
+
 /* function to generate a random graph */
-unsigned long int ** random_graph(unsigned long int ** tableau) {
+int ** random_graph() {
 	
-	unsigned long int i, j, nb_followers, value, sommet;
-	tableau = (unsigned long int **) calloc(nb_summit, sizeof(unsigned long int *));
+	unsigned long int i, j;
 	
-	/* allocate space for our little matrix, and initialize the diagonal */
+	int **tableau = (int **) calloc(nb_summit, sizeof(unsigned long int *));
+	
+	/* allocate space for our little matrix */
 	for (i=0; i<nb_summit; i++) {
-		tableau[i] = (unsigned long int *) calloc(nb_summit, sizeof(unsigned long int));
-		tableau[i][i] = INFI_LOCAL;
+		tableau[i] = (int *) calloc(nb_summit, sizeof(unsigned long int));
 	}
 	
-	/* generate random arc with its value*/
+	/* generate random arc with its value */
 	for (i=0;i<nb_summit;i++){
-		/* generate a random number of followers */
-		nb_followers = (int) round(rand()/(RAND_MAX + 1.) * nb_summit * REMPLISSAGE);
-		j=0;
 		/* generate the cost of the arc */
-		while (j<nb_followers) {
-			sommet = (int) round(rand()/(RAND_MAX + 1.) * nb_summit);
-			/* if tableau !=0 it means we already used this summit, as calloc initialize
-			 * to zero the cells of the matrix */
-			if (tableau[i][sommet] == 0) {
-				value = (unsigned long int) (1 + round(rand()/(RAND_MAX + 1.) * (INFI_LOCAL -1 )));
-				tableau[i][sommet] = value;
-				printf("%ld\n", tableau[i][sommet]);
-				j++;
+		for (j=0; j<nb_summit; j++) {
+			if (j != i) {
+				/* we fill the value only if we are under the FILLING percentage */
+				if (rand()/(RAND_MAX + 1.) < FILLING) {
+					tableau[i][j] = (int) (1 + round(rand()/(RAND_MAX + 1.) * (INFI_LOCAL -1 )));
+				}
+				else {
+					tableau[i][j] = INFI_LOCAL;
+				}
+
+			}
+			else {
+				tableau[i][i] = 0;
 			}
 		}
 	}
@@ -73,10 +97,8 @@ int main (int argc, const char * argv[]) {
 	
 	/* first we use a matrix to generate the graph. then we will have a function
 	 to make the conversion to the struct */
-	unsigned long int **adj_mat;
 	
-	adj_mat = random_graph(adj_mat);
-	print_matrix(adj_mat);
+	print_matrix(random_graph());
 	
     return 0;
 }
