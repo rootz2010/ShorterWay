bc0bfea5db8d8ef10d79cb48e9563b6cbaf7c7b7
Ludovic Delaveau <ldelaveau@gmail.com>
2010-04-20 12:51:21 +0200

    Matrix to Chained List conversion method done, surely not very clean.
    Chained List display method also written, same remark.
    
    There is a "bus error" coming sometimes, not corrected at the moment,
    and I didn't find the cause yet.
    
    Changed some variables name, and a few data types

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int nb_summit = 10;
 
 /* simple data structure : chained-list containing the number of the summit */
 struct chained_list {
-	unsigned long int numero;
+	unsigned long int number;
 	int value;
 	struct chained_list * next;
 };
@@ -19,23 +19,39 @@ struct chained_list {
 /* function to save the datas in a file */
 
 /* function to convert a matrix into a chained list */
-struct chained_list * convert_matrix(int ** tab) {
+struct chained_list ** convert_matrix(int ** tab) {
 	unsigned long int i, j;
 	struct chained_list * pointer;
 	
+	/* we allocate nb_summit cells for the chained list tab */
 	struct chained_list **list = (struct chained_list **) calloc(nb_summit, sizeof(struct chained_list *));
 	
 	for (i=0; i<nb_summit; i++) {
+		list[i]= NULL;
 		for (j=0; j<nb_summit; j++) {
-			if (j != i && tab[i][j] != 0) {
-				pointer->numero = j;
-				/* have a pointer to the current editing struct, as done in a stack ? */
-				
+			/* to convert the matrix we want to take only the cells set */
+			if (j != i && tab[i][j] != INFI_LOCAL) {
+				/* if list[i] is NULL, it means we have to allocate a struct in it */
+				if (list[i] == NULL) {
+					list[i] = (struct chained_list *) malloc(sizeof(struct chained_list));
+					/* we copy the address of the new struct in the pointer */
+					pointer = list[i];
+				}
+				/* else it means we already filled the cell with at least one value, and we have
+				 * created a pointer */
+				else {
+					pointer->next = (struct chained_list *) malloc(sizeof(struct chained_list));
+					pointer = pointer->next;
+				}
+				/* storing time ! */
+				pointer->number = j;
+				pointer->value = tab[i][j];
 			}
 		}
+		pointer->next = NULL;
 	}
 	
-	return NULL;
+	return &list[0];
 }
 
 /* function to convert from chained_list to matrix ? is there a need for it ? */
@@ -43,7 +59,7 @@ struct chained_list * convert_matrix(int ** tab) {
 /* function to print the matrix in the terminal ... simple displaying method */
 void print_matrix(int ** tableau) {
 	
-	int i, j;
+	unsigned long int i, j;
 	
 	for (i=0; i<nb_summit; i++) {
 		for (j=0; j<nb_summit; j++) {
@@ -53,7 +69,23 @@ void print_matrix(int ** tableau) {
 	}
 }
 
-/* function to print a chained_list, maybe as a tab ? */
+/* function to print a chained_list */
+void print_chained_list(struct chained_list ** list) {
+	unsigned long int i;
+	struct chained_list * pointer;
+	
+	for (i=0; i<nb_summit; i++) {
+		printf("summit %ld\t", i + 1);
+		pointer = list[i];
+		/* while we have something to display, we just display it ...*/
+		while (pointer != NULL) {
+			printf("d(%ld,%ld)=%d\t", i + 1, (pointer->number) + 1, pointer->value);
+			pointer = pointer->next;
+		}
+		printf("\n");
+	}
+}
+
 
 /* function to generate a random graph */
 int ** random_graph() {
@@ -98,7 +130,9 @@ int main (int argc, const char * argv[]) {
 	/* first we use a matrix to generate the graph. then we will have a function
 	 to make the conversion to the struct */
 	
-	print_matrix(random_graph());
-	
+	int ** tableau;
+	tableau = random_graph();
+	print_matrix(tableau);
+	print_chained_list(convert_matrix(tableau));
     return 0;
 }
